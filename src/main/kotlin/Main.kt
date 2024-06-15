6c2e6b5022992ca87a9f803505784214fdc3a25d
Kovalski <andre.kovalski@tecpuc.com.br>
2024-06-15 05:00:14 -0300

    updating assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 import kotlin.random.Random
+import org.jetbrains.letsPlot.export.ggsave
+import org.jetbrains.letsPlot.geom.geomBar
+import org.jetbrains.letsPlot.geom.geomQQ
+import org.jetbrains.letsPlot.geom.geomQQLine
+import org.jetbrains.letsPlot.ggplot
+import org.jetbrains.letsPlot.label.ggtitle
+import org.jetbrains.letsPlot.letsPlot
 
 val rota_direta_distancia = 6.24
 val rota_alternativa_distancia = 7.3
@@ -25,7 +32,7 @@ fun main() {
         simulaRotaSegura()
     }
 
-    val pesoTemposRotaDireta = temposRotaDireta.reduce{ acc, i -> acc+i }*0.15
+    val pesoTemposRotaDireta = temposRotaDireta.sum()*0.15
     val pesoConsumoRotaDireta = consumosRotaDireta.reduce{ acc, i -> acc+i }*0.85
     val valorRotaDireta = pesoTemposRotaDireta+pesoConsumoRotaDireta
 
@@ -41,29 +48,29 @@ fun main() {
     println("Valor geral rota direta: %.2f".format(valorRotaDireta/normalizado))
     println("Valor geral rota alternativa: %.2f".format(valorRotaAlternativa/normalizado))
     println("Valor geral rota segura: %.2f".format(valorRotaSegura/normalizado))
+    plotQQ(consumosRotaDireta, "consumoRotaDireta")
+    plotQQ(temposRotaDireta, "temposRotaDireta")
     println()
     normalizado = pesoTemposRotaDireta+pesoTemposRotaAlternativa+pesoTemposRotaSegura
     println("Valor consumo combustível rota direta: %.2f".format(pesoTemposRotaDireta/normalizado))
     println("Valor consumo combustível rota alternativa: %.2f".format(pesoTemposRotaAlternativa/normalizado))
     println("Valor consumo combustível rota segura: %.2f".format(pesoTemposRotaSegura/normalizado))
+    plotQQ(consumosRotaAlternativa, "consumosRotaAlternativa")
+    plotQQ(temposRotaAlternativa, "temposRotaAlternativa")
     println()
     normalizado = pesoConsumoRotaDireta+pesoConsumoRotaAlternativa+pesoConsumoRotaSegura
     println("Valor tempo rota direta: %.2f".format(pesoConsumoRotaDireta/normalizado))
     println("Valor tempo rota alternativa: %.2f".format(pesoConsumoRotaAlternativa/normalizado))
     println("Valor tempo rota segura: %.2f".format(pesoConsumoRotaSegura/normalizado))
+    plotQQ(consumosRotaSegura, "consumosRotaSegura")
+    plotQQ(temposRotaSegura, "temposRotaSegura")
 
 }
 
-enum class Rota {
-    DIRETA, ALTERNATIVA, SEGURA
-}
-fun getRotaEscolhida(): Rota{
-    val rota = Random.nextInt(0, 3)
-    return when (rota) {
-        0 -> Rota.DIRETA
-        1 -> Rota.ALTERNATIVA
-        else -> Rota.SEGURA
-    }
+fun plotQQ(ys: List<Number>, outputName: String, title: String = "", subTitle: String = "") {
+    val data = mapOf<String, Any>("x" to List(ys.size){ it+1 }, "y" to ys)
+    val fig = letsPlot(data) {sample = ys} + geomQQ(size = 3, alpha = .3) + geomQQLine(size = 1) + ggtitle(title, subTitle)
+    ggsave(fig, "$outputName.png")
 }
 
 fun getRandomBetween0And1(): Double {
